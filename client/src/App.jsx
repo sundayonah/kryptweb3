2f4a56bfc402cc2381d922897a2ff9ab5bd69d60
sundayonah <sundayonah94@gmail.com>
2023-06-25 15:46:25 +0100

    folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-function App() {
 
+const App = () => {
   return (
-    <div className="flex justify-between align-center">
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <p className="text-red-500">hi man</p>
-    </div>
+    <div>App</div>
   )
 }
+
 export default App
