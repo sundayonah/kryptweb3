9ed28871f8706681b690ad789b7ebacd0d9f15e9
sundayonah <sundayonah94@gmail.com>
2023-06-25 17:11:36 +0100

    navbar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import {Navbar, Welcome, Footer, Services, Transactions} from './components';
 const App = () => {
   return (
     <div className='min-h-screen'>
-      <div className='gradient-bg-welcom'>
+      <div className='gradient-bg-welcome'>
       <Navbar />
       <Welcome />
       </div>
