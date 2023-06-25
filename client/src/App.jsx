27dc9f1458c3adc78b3d21a9041d1edce0a520e5
sundayonah <sundayonah94@gmail.com>
2023-06-25 15:15:26 +0100

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="flex justify-between align-center">
+       <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <p className="text-red-500">hi man</p>
+    </div>
+  )
+}
+export default App
